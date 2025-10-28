c77c5933c433c6a965982af062671555e140be4e
73262 <nikosjas2005@gmail.com>
2025-10-28 12:27:42 +0100

    zad4

---
+++ Lab1/zad5.py
@@ -0,0 +1,5 @@
+a = float(input("Podaj długość pierwszego boku "))
+b = float(input("Podaj długość drugiego boku:"))
+pole = a*b
+obw = 2*(a+b)
+print(f"Pole prostokata o wymiarach {a} na {b} wynosi: {pole}")
